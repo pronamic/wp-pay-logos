fd340650b38870886d2535e8e910882e0709e9d2
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-07-15 14:48:10 +0200

    Docs.

---
+++ docs/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Pronamic Pay Logos</title>
+
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+	</head>
+
+	<body>
+		<div class="px-4 py-5 my-5 text-center">
+			<h1 class="display-5 fw-bold">Pronamic Pay Logos</h1>
+
+			<div class="col-lg-6 mx-auto">
+				<p class="lead mb-4">Logos for the Pronamic Pay WordPress plugin.</p>
+			</div>
+		</div>
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+	</body>
+</html>
